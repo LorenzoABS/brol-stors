d78431c278cce39be555db7c8fb6ea5f28217379
LorenzoABS <150017251+LorenzoABS@users.noreply.github.com>
2024-01-11 15:16:37 +0100

    javascript

---
+++ index.js
@@ -0,0 +1,325 @@
+const characters = [
+{
+    name: 'shelly',
+    characterImage: 'image/shelly.png'
+},
+{
+    name: 'larry & lawrie',
+    characterImage: 'image/larry & lawrie.png'
+},
+{
+    name: 'el primo',
+    characterImage: 'image/el primo.png'
+},
+{
+    name: 'nita',
+    characterImage: 'image/nita.png'
+},
+{
+    name: 'jessie',
+    characterImage: 'image/jessie.png'
+},
+{
+    name: 'poco',
+    characterImage: 'image/poco.png'
+},
+{
+    name: 'jacky',
+    characterImage: 'image/jacky.png'
+},
+{
+    name: 'colt',
+    characterImage: 'image/colt.png'
+},
+{
+    name: 'brock',
+    characterImage: 'image/brock.png'
+},
+{
+    name: 'surge',
+    characterImage: 'image/surge.png'
+},
+{
+    name: 'barley',
+    characterImage: 'image/barley.png'
+},
+{
+    name: 'bull',
+    characterImage: 'image/bull.png'
+},
+{
+    name: 'emz',
+    characterImage: 'image/emz.png'
+},
+{
+    name: 'dynamike',
+    characterImage: 'image/dynamike.png'
+},
+{
+    name: 'fang',
+    characterImage: './images/fang.jpg'
+},
+{
+    name: 'rosa',
+    characterImage: 'image/rosa.png'
+},
+{
+    name: 'lou',
+    characterImage: 'image/lou.png'
+},
+{
+    name: 'bo',
+    characterImage: 'image/bo.png'
+},
+{
+    name: 'darryl',
+    characterImage: 'image/darryl.png'
+},
+{
+    name: '8-bit',
+    characterImage: 'image/8-bit.png'
+},
+{
+    name: 'frank',
+    characterImage: 'image/frank.png'
+},
+{
+    name: 'bea',
+    characterImage: 'image/bea.png'
+},
+{
+    name: 'ruffs',
+    characterImage: 'image/ruffs.png'
+},
+{
+    name: 'penny',
+    characterImage: 'image/penny.png'
+},
+{
+    name: 'buzz',
+    characterImage: 'image/buzz.png'
+},
+{
+    name: 'carl',
+    characterImage: 'image/carl.png'
+},
+{
+    name: 'tick',
+    characterImage: 'image/tick.png'
+},
+{
+    name: 'eve',
+    characterImage: 'image/eve.png'
+},
+{
+    name: 'stu',
+    characterImage: 'image/stu.png'
+},
+{
+    name: 'pam',
+    characterImage: 'image/pam.png'
+},
+{
+    name: 'mortis',
+    characterImage: 'image/mortis.png'
+},
+{
+    name: 'gene',
+    characterImage: 'image/gene.png'
+},
+{
+    name: 'sprout',
+    characterImage: 'image/sprout.png'
+},
+{
+    name: 'gray',
+    characterImage: 'image/gray.png'
+},
+{
+    name: 'edgar',
+    characterImage: 'image/edgar.png'
+},
+{
+    name: 'rico',
+    characterImage: 'image/rico.png'
+},
+{
+    name: 'grom',
+    characterImage: 'image/grom.png'
+},
+{
+    name: 'mr.p',
+    characterImage: 'image/mr.p.png'
+},
+{
+    name: 'griff',
+    characterImage: 'image/griff.png'
+},
+{
+    name: 'otis',
+    characterImage: 'image/otis.png'
+},
+{
+    name: 'tara',
+    characterImage: 'image/tara.png'
+},
+{
+    name: 'janet',
+    characterImage: 'image/janet.png'
+},
+{
+    name: 'gus',
+    characterImage: 'image/gus.png'
+},
+{
+    name: 'byron',
+    characterImage: 'image/byron.png'
+},
+{
+    name: 'bonnie',
+    characterImage: 'image/bonnie.png'
+},
+{
+    name: 'max',
+    characterImage: 'image/max.png'
+},
+{
+    name: 'cordelius',
+    characterImage: 'image/cordelius.png'
+},
+{
+    name: 'squak',
+    characterImage: 'image/squak.png'
+},
+{
+    name: 'bibi',
+    characterImage: 'image/bibi.png'
+},
+{
+    name: 'piper',
+    characterImage: 'image/piper.png'
+},
+{
+    name: 'nani',
+    characterImage: 'image/nani.png'
+},
+{
+    name: 'gale',
+    characterImage: 'image/gale.png'
+},
+{
+    name: 'buster',
+    characterImage: 'image/buster.png'
+},
+{
+    name: 'R-T',
+    characterImage: 'image/R-T.png'
+},
+{
+    name: 'sandy',
+    characterImage: 'image/sandy.png'
+},
+{
+    name: 'hank',
+    characterImage: 'image/hank.png'
+},
+{
+    name: 'amber',
+    characterImage: 'image/amber.png'
+},
+{
+    name: 'chester',
+    characterImage: 'image/chester.png'
+},
+{
+    name: 'crow',
+    characterImage: 'image/crow.png'
+},
+{
+    name: 'collette',
+    characterImage: 'image/collette.png'
+},
+{
+    name: 'meg',
+    characterImage: 'image/meg.png'
+},
+{
+    name: 'leon',
+    characterImage: 'image/leon.png'
+},
+{
+    name: 'belle',
+    characterImage: 'image/belle.png'
+},
+{
+    name: 'wilow',
+    characterImage: 'image/wilow.png'
+},
+{
+    name: 'spike',
+    characterImage: 'image/shelly.png'
+},
+{
+    name: 'ash',
+    characterImage: 'image/shelly.png'
+},
+{
+    name: 'lola',
+    characterImage: 'image/shelly.png'
+},
+{
+    name: 'doug',
+    characterImage: 'image/shelly.png'
+},
+{
+    name: 'sam',
+    characterImage: 'image/shelly.png'
+},
+{
+    name: 'chuck',
+    characterImage: 'image/shelly.png'
+},
+{
+    name: 'mandy',
+    characterImage: 'image/shelly.png'
+},
+{
+    name: 'maisie',
+    characterImage: 'image/shelly.png'
+},
+{
+    name: 'charlie',
+    characterImage: 'image/shelly.png'
+},
+{
+    name: 'pearl',
+    characterImage: 'image/shelly.png'
+},
+{
+    name: 'mico',
+    characterImage: 'image/shelly.png'
+},
+{
+    name: 'kit',
+    characterImage: 'image/shelly.png'
+},
+
+
+
+
+]
+
+const h4 = document.querySelector('h4')
+const img = document.querySelector('img')
+img.style.animationPlayState = 'paused'
+
+img.onclick = () => {
+    img.style.animationPlayState = 'running'
+    const randomIndex = Math.floor(Math.random() * characters.length)
+    const { name, characterImage } = characters[randomIndex]
+    setTimeout(() => {
+        img.setAttribute('src', characterImage)
+        h4.innerText = name
+    }, 3000)
+}
+
